439d45cf187abafa7db5762ea29cadaabd691a31
Nadeem Sheriff A <137400851+nadeemsheriff18@users.noreply.github.com>
2024-05-10 18:44:23 +0530

    header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from  'react';
 import Gameboard from './Gameboard';
+import Navbar from './Navbar';
 
 function App(){
     return (
         <div>
-       
+           <Navbar />
            <Gameboard />
         </div>
     )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CasinoIcon from '@mui/icons-material/Casino';
+
+const Navbar = () => {
+  return (
+    <div className='navbar' >
+    <CasinoIcon className='icon' fontSize='large'/>
+        <h1>CONNECT-4</h1>
+    </div>
+  )
+}
+
+export default Navbar
